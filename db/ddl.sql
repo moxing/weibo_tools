16bcfba5d887eb1cc29590ac059792e592badcc8
liu tao <star2ko@gmail.com>
2013-08-16 18:19:39 +0800

    DDL update

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,7 +1,6 @@
 CREATE TABLE `wb_status` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `wb_id` varchar(30) NOT NULL,
-  `wb_ori_id` varchar(30) DEFAULT NULL,
+  `id` varchar(30) NOT NULL,
+  `ori_id` varchar(30) DEFAULT NULL,
   `uid` varchar(30) NOT NULL,
   `text` varchar(300) NOT NULL,
   `status_datetime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -9,37 +8,29 @@ CREATE TABLE `wb_status` (
   `bmiddle_pic` varchar(100) DEFAULT NULL,
   `original_pic` varchar(100) DEFAULT NULL,
   `pic_urls` text,
+  `status` smallint(6) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `idx_id` (`wb_id`) USING BTREE,
+  UNIQUE KEY `idx_id` (`id`) USING BTREE,
   KEY `idx_t` (`uid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-
-CREATE TABLE `wb_task` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `since_id` varchar(30) NOT NULL,
-  `current_id` varchar(30) NOT NULL,
-  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `wb_token` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` varchar(30) NOT NULL,
   `access_token` varchar(100) NOT NULL,
   `expires_in` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`uid`),
   UNIQUE KEY `user_id` (`uid`),
   KEY `idx_t` (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `wb_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` varchar(30) NOT NULL,
-  `expires_in` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `id` varchar(30) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `profile_image_url` varchar(100) NOT NULL DEFAULT '0000-00-00 00:00:00',
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `user_id` (`uid`),
-  KEY `idx_t` (`uid`)
+  UNIQUE KEY `user_id` (`id`),
+  KEY `idx_t` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
